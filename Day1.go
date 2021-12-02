651569a0c684f04cc46f9cfbcbf8a4e7d1cfb184
Max den Hartog <max.den.hartog@hotmail.com>
2021-12-02 11:28:15 +0100

    Day 2 solutions

--- Day1.go
+++ Day1.go
@@ -7,11 +7,12 @@ import (
 )
 
 func (function *Function) Day1_1() {
-	data, len := helpers.ReadLines("input/01.txt")
+	data, length := helpers.ReadLines("input/01.txt")
 	parsedData := helpers.LinesAsInt(data)
+
 	result := 0
 
-	for i := 1; i < len; i++ {
+	for i := 1; i < length; i++ {
 		if parsedData[i] > parsedData[i-1] {
 			result++
 		}
@@ -21,13 +22,13 @@ func (function *Function) Day1_1() {
 }
 
 func (function *Function) Day1_2() {
-	data, len := helpers.ReadLines("input/01.txt")
+	data, length := helpers.ReadLines("input/01.txt")
 	parsedData := helpers.LinesAsInt(data)
-	result := 0
 
+	result := 0
 	window := parsedData[0] + parsedData[1] + parsedData[2]
 
-	for i := 1; i < len-2; i++ {
+	for i := 1; i < length-2; i++ {
 		nextWindow := window - parsedData[i-1] + parsedData[i+2]
 		if nextWindow > window {
 			result++
---
+++ Day2.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/MaxdenHartog/AoC2021/helpers"
+)
+
+func (function *Function) Day2_1() {
+	data, length := helpers.ReadLines("input/02.txt")
+
+	instructions := make(map[string]int)
+
+	for i := 0; i < length; i++ {
+		instruction := string(data[i][0])
+		distance, _ := strconv.Atoi(string(data[i][len(data[i])-1]))
+
+		instructions[instruction] += distance
+	}
+
+	result := instructions["f"] * (instructions["d"] - instructions["u"])
+
+	fmt.Println(result)
+}
+
+func (function *Function) Day2_2() {
+	data, length := helpers.ReadLines("input/02.txt")
+
+	instructions := make(map[string]int)
+
+	for i := 0; i < length; i++ {
+		instruction := string(data[i][0])
+		distance, _ := strconv.Atoi(string(data[i][len(data[i])-1]))
+
+		if instruction == "f" {
+			instructions["depth"] += distance * (instructions["d"] - instructions["u"])
+		}
+
+		instructions[instruction] += distance
+	}
+
+	result := instructions["f"] * instructions["depth"]
+
+	fmt.Println(result)
+}
