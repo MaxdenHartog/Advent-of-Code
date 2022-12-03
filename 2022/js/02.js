5b8afc9ae0f3ec51a1ca3e9c30df626248857250
Max den Hartog <max.den.hartog@hotmail.com>
2022-12-03 10:52:32 +0100

    Added day 2 second puzzle

--- 2022/js/02.js
+++ 2022/js/02.js
@@ -1,5 +1,12 @@
-var fs = require('fs');
-var array = fs.readFileSync('input.txt').toString().split("\n");
+var io = require('./helpers/io');
+var input = io.readLines('./input/02.txt');
+
+day2_1();
+day2_2();
+
+// A X Rock
+// B Y Paper
+// C Z Scissors
 
 // A Rock 1
 // B Paper 2
@@ -7,50 +14,103 @@ var array = fs.readFileSync('input.txt').toString().split("\n");
 // X lose 0
 // Y draw 3
 // Z win 6
-let total = 0;
-for(i in array) {
-    switch(array[i][2]){
-        case 'X':
-            if(array[i][0] == 'A'){
-                total += 3;
-                break;
-            }
-            else if(array[i][0] == 'B'){
+
+function day2_1(){
+    let total = 0;
+    for(let i = 0; i < input.length; i++){
+        switch(input[i][2]){
+            case 'X':
                 total += 1;
-                break;
-            }
-            else {
+                if (input[i][0] == 'A'){
+                    total += 3;
+                    break;
+                }
+                else if (input[i][0] == 'B') {
+                    break;
+                }
+                else {
+                    total += 6;
+                    break;
+                }
+            case 'Y':
                 total += 2;
-                break;
-            }
-        case 'Y':
-            total += 3;
-            if(array[i][0] == 'A'){
-                total += 1;
-                break;
-            }
-            else if(array[i][0] == 'B'){
-                total += 2;
-                break;
-            }
-            else {
+                if (input[i][0] == 'A'){
+                    total += 6;
+                    break;
+                }
+                else if (input[i][0] == 'B') {
+                    total += 3;
+                    break;
+                }
+                else {
+                    break;
+                }
+            case 'Z':
                 total += 3;
-                break;
-            }
-        case 'Z':
-            total += 6;
-            if(array[i][0] == 'A'){
-                total += 2;
-                break;
-            }
-            else if(array[i][0] == 'B'){
-                total += 3;
-                break;
-            }
-            else {
-                total += 1;
-                break;
-            }        
+                if (input[i][0] == 'A'){
+                    break;
+                }
+                else if (input[i][0] == 'B') {
+                    total += 6;
+                    break;
+                }
+                else {
+                    total += 3;
+                    break;
+                }
+        }
     }
+    console.log("Puzzle 1:");
+    console.log(total);
 }
-console.log(total);
+
+function day2_2(){
+    let total = 0;
+    for(i in array) {
+        switch(array[i][2]){
+            case 'X':
+                if(array[i][0] == 'A'){
+                    total += 3;
+                    break;
+                }
+                else if(array[i][0] == 'B'){
+                    total += 1;
+                    break;
+                }
+                else {
+                    total += 2;
+                    break;
+                }
+            case 'Y':
+                total += 3;
+                if(array[i][0] == 'A'){
+                    total += 1;
+                    break;
+                }
+                else if(array[i][0] == 'B'){
+                    total += 2;
+                    break;
+                }
+                else {
+                    total += 3;
+                    break;
+                }
+            case 'Z':
+                total += 6;
+                if(array[i][0] == 'A'){
+                    total += 2;
+                    break;
+                }
+                else if(array[i][0] == 'B'){
+                    total += 3;
+                    break;
+                }
+                else {
+                    total += 1;
+                    break;
+                }        
+        }
+    }
+    console.log("Puzzle 2:");
+    console.log(total);
+}
