abdcfb7637dcc9d8c830379339957cb9472f75bd
Max den Hartog <max.den.hartog@hotmail.com>
2022-12-04 10:01:39 +0100

    Added day 5 in advance

---
+++ 2022/js/05.js
@@ -0,0 +1,21 @@
+var io = require('../../helpers/js/io');
+var input = io.readLines('../input/test.txt');
+
+//day5_1();
+//day5_2();
+
+function day5_1(){
+    let result = 0;
+    for(let i = 0; i < input.length; i++){
+    }
+    console.log("Puzzle 1:");
+    console.log(result);
+}
+
+function day5_2(){
+    let result = 0;
+    for(let i = 0; i < input.length; i++){
+    }
+    console.log("Puzzle 2:");
+    console.log(result);
+}
