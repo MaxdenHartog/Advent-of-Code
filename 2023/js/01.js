8f02e7a3d7aa04a064adb05545f1b3b923e37811
Max den Hartog <Max.den.Hartog@hotmail.com>
2023-12-01 12:12:10 +0100

    Add 2023-1-1 and prep for part 2

--- 2023/js/01.js
+++ 2023/js/01.js
@@ -1,28 +1,5 @@
 var io = require('../../helpers/js/io');
-var input = io.readLines('../input/01.txt');
-
-day1_1();
-day1_2();
-
-function day1_1(){
-    let sum = 0;
-    for (let i = 0; i < input.length; i++){
-        let calibtration = input[i].match(/\d/g)
-        calibtration = calibtration[0] + calibtration[calibtration.length - 1]
-        sum += parseInt(calibtration);
-    }
-    console.log("Puzzle 1:");
-    console.log(sum);
-}
-
-function day1_2(){
-    let sum = 0;
-    for (let i = 0; i < input.length; i++){
-        numbers.some(function(n) {return input[i].includes(n)})
-    }
-    console.log("Puzzle 2:");
-    console.log(sum);
-}
+var input = io.readLines('../input/test.txt');
 
 const numbers = [
     "zero",
@@ -36,3 +13,33 @@ const numbers = [
     "eight",
     "nine"
 ]
+
+day1_1();
+day1_2();
+
+function day1_1(){
+    let sum = 0;
+    for (let i = 0; i < input.length; i++){
+        let calibration = input[i].match(/\d/g)
+        calibration = calibration[0] + calibration[calibration.length - 1]
+        sum += parseInt(calibration);
+    }
+    console.log("Puzzle 1:");
+    console.log(sum);
+}
+
+function day1_2(){
+    let sum = 0;
+    for (let i = 0; i < input.length; i++){
+        let calibration = input[i].match(/\d|(zero)|(one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine)/g);
+        let first = !isNaN(parseInt(calibration[0])) ?
+            parseInt(calibration[0]) * 10 :
+            numbers.indexOf(calibration[0]) * 10;
+        let last = !isNaN(parseInt(calibration[calibration.length - 1])) ?
+            parseInt(calibration[calibration.length - 1]) :
+            numbers.indexOf(calibration[calibration.length - 1]);
+        sum += first + last;
+    }
+    console.log("Puzzle 2:");
+    console.log(sum);
+}
