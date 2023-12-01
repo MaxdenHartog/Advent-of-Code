aa8f21394f9f09f0fd9014461f26a25ac0e5d8e1
Max den Hartog <Max.den.Hartog@hotmail.com>
2023-12-01 11:06:23 +0100

    Add 2023-1-1 and prep for part 2

---
+++ 2023/js/01.js
@@ -0,0 +1,36 @@
+var io = require('../../helpers/js/io');
+var input = io.readLines('../input/01.txt');
+
+day1_1();
+day1_2();
+
+function day1_1(){
+    let sum = 0;
+    for (let i = 0; i < input.length; i++){
+        let calibtration = input[i].match(/\d/g)
+        calibtration = calibtration[0] + calibtration[calibtration.length - 1]
+        sum += parseInt(calibtration);
+    }
+    console.log("Puzzle 1:");
+    console.log(sum);
+}
+
+function day1_2(){
+    let sum = 0;
+
+    console.log("Puzzle 2:");
+    console.log(sum);
+}
+
+const numbers = [
+    "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine"
+]
